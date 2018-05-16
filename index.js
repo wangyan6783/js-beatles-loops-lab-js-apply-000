1e75fe192eac0568ac22eac1c96bb9f3ccaca7ee
Yan Wang <wangyan6783@gmail.com>
2018-05-16 14:55:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var newArr = [];
-  for (var i = 0; i < musicians.lenth; i++) {
+  for (var i = 0; i < musicians.length; i++) {
     var musician = musicians[i];
     var instrument = instruments[i];
     newArr.push(musician + ' plays ' + instrument);
